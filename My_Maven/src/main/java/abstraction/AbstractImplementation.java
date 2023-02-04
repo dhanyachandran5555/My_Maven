9fceb0f9728125970703a0fa3f2ce3ed41196211
dhany <dhany@Dhanya>
2023-02-04 21:46:55 +0530

    My First (My_Maven) Commit

---
+++ My_Maven/src/main/java/abstraction/AbstractImplementation.java
@@ -0,0 +1,7 @@
+package abstraction;
+
+public class AbstractImplementation extends Student {
+	public void process() {
+		System.out.println("Printing Process Method");// Method Overriding
+	}
+}
---
+++ My_Maven/src/main/java/abstraction/AbstractionMain.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public class AbstractionMain {
+
+	public static void main(String[] args) {
+		AbstractImplementation a1 = new AbstractImplementation();
+		a1.process();
+		a1.setUp();
+		a1.tearDown();
+	}
+
+}
